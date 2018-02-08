5b2580f90ec95ee2112b63c06bef554d0c1ce8e6
tukallo <alex.tukallo@gmail.com>
2018-02-08 10:56:00 +0300

    Major: github api interaction added

--- analyzis.py
+++ analyzis.py
@@ -2,28 +2,30 @@ import vk_api
 from github import Github
 
 KEY_WORDS_PATH = "key_words"
-TECHNOLOGIES_PATH = "languages"
+LANGUAGES_PATH = "languages"
 COMPANIES_PATH = "companies"
 
 KEY_WORDS = []
-TECHNOLOGIES = []
+LANGUAGES = []
 COMPANIES = []
 
 
 def read_files():
-    global KEY_WORDS, TECHNOLOGIES, COMPANIES
+    global KEY_WORDS, LANGUAGES, COMPANIES
 
     def read(path):
         with open(path) as file:
             return list(filter(len, [line.strip().lower() for line in file]))
 
     KEY_WORDS = read(KEY_WORDS_PATH)
-    TECHNOLOGIES = read(TECHNOLOGIES_PATH)
+    LANGUAGES = read(LANGUAGES_PATH)
     COMPANIES = read(COMPANIES_PATH)
 
 
 read_files()
-print("Read data from files")
+print("Read utils from files")
+g = Github(login_or_token="a30aaccea5624f96d8650542b7a86f56b66c6b1a")
+print("Authorized with public key to github")
 
 
 def contains_any(substring: str, strings: list):
@@ -42,26 +44,28 @@ def analyze_user(id, vkk):
     vk = vkk
 
     rating = 0
-    lang = ""
+
+    lang_stats = {}
+    for l in LANGUAGES:
+        lang_stats[l] = 0
+
     try:
         user_info = vk.users.get(user_ids=id, fields='career,universities,site')[0]
-        print('Analyzing ' + user_info['first_name'] + ' ' + user_info['last_name'])
+        print('Analyzing ' + user_info['first_name'] + ' ' + user_info['last_name'] +
+              ' query answer: ' + str(user_info))
 
-        # print(user_info)
-
-        career = []
         if 'career' in user_info:
             career = user_info['career']
-            rating += analyze_job(career)
+            rating += analyze_job(career, lang_stats)
 
         if 'universities' in user_info:
             rating += analyze_education(user_info['universities'])
 
         if 'site' in user_info:
-            rating += analyze_github(user_info['site'])
+            rating += analyze_github(user_info['site'], lang_stats)
 
         groups = vk.groups.get(user_id=id, filter='groups,publics', extended=1, fields='name')
-        rating += analyze_groups(groups)
+        rating += analyze_groups(groups, lang_stats)
 
         print('Got rating ' + str(rating))
         return min(rating, 10)
@@ -76,31 +80,35 @@ def analyze_language(groups, career):
 
 
 # gives max of 3
-def analyze_groups(groups):
+def analyze_groups(groups, lang_stats):
+    rating = 0
     print(groups)
-    for group in groups:
+    for group in groups['items']:
+        print(group)
         pass
-        # todo:
-        # if contains_any()
-    pass
+    return min(rating, 3)
 
 
 # gives max of 3
-def analyze_job(career):
+def analyze_job(career, lang_stats):
     score = 0
     for job in career:
         if 'position' in job and contains_any(job['position'], KEY_WORDS):
             score += 1
+            # let's analyze some technologies:
+            for l in LANGUAGES:
+                if job['position'].find(l) != -1:
+                    lang_stats[l] += 1
+
         if 'company' in job and contains_any(job['company'], COMPANIES):
             score += 1
         elif 'group_id' in job:
             group = vk.groups.getById(group_id=job['group_id'])
             if 'name' in group and contains_any(group['name'], COMPANIES):
                 score += 1
-    return min(score, 3)
+    return min(score, 3)  # gives max of 3
 
 
-# gives max of 3
 def analyze_education(education):
     score = 0
     for uni in education:
@@ -114,11 +122,22 @@ def analyze_education(education):
 
 
 # gives 5 if present. Person with a github is definitely a programmer!
-def analyze_github(site: str):
-    # todo: github api for language
-    if site.find('github') != -1:
+def analyze_github(site: str, lang_stats: dict):
+    if site.find('github') == -1:
+        return 0
 
-        Github.get_user()
-
-        return 5
-    return 0
+    # let's try to parse the user's name
+    login = site.replace('https:', '').replace('github', '').replace('/', '') \
+        .replace('.', '').replace('com', '').replace('io', '')
+    print('Loading github repos for login: ' + login)
+    # learn favourite language from github repos
+    try:
+        for r in g.get_user(login).get_repos():
+            l = r.language.lower()
+            if l in lang_stats:
+                lang_stats[l] += 1
+            else:
+                print('Language ' + l + ' is ignored')
+    except Exception as e:
+        print(e)
+    return 5  # return score of 5 for rating
--- preprocess.py
+++ preprocess.py
@@ -1,39 +1,42 @@
 import collections
 
+import pandas as pd
 import vk_api
 
 import analyzis
 
-import pandas as pd
-
 vk = None
 default_user = ('125376958', 'Alexander', 'Tukallo')
 
-SCORE_THRESHOLD = 3  # score is given from 0 to 10. If score >= threshold => is programmer
+SCORE_THRESHOLD = 3  # score is given from 0 to 10. If score >= threshold => user is a programmer
 
 
-# Method returns pair: (rating : int, language : str),
-#   where rating is an int in a range [0; 10]
-#   depicting assurance, that a user is a programmer
-#   language is user's favourite programming language if any, else ""
 def get_score(id):
-    return analyzis.analyze_user(id, vk), "c++"
+    """
+    :param id: user_id to get_score for
+    :return: (rating: ing, language: str), where rating is an int in a range [0; 10]
+            depicting assurance, that a user is a programmer,
+            language is user's favourite programming language if any, else "-"
+    """
+    return analyzis.analyze_user(id, vk), "-"
 
 
-def walk_and_store(start=default_user, limit=5, lang=""):
+def walk_and_store(start=default_user, limit=200, lang=""):
     programmers = pd.DataFrame(columns=('id', 'first_name', 'last_name', 'rank', 'language', 'link'))
-    visited_ids = set()
+
+    observed_users = set()
+    observed_users.add(start)
+
     to_visit = collections.deque()
     to_visit.append(start)
 
     # starting bfs
     while len(to_visit) != 0:
         # logging:
-        # if len(visited_ids) % 5 == 0:
-        #     print('Visited ' + str(len(visited_ids)) + ' guys')
+        # if len(observed_users) % 5 == 0:
+        #     print('Visited ' + str(len(observed_users)) + ' guys')
 
         cur_user = to_visit.popleft()  # (id, first_name, last_name)
-        visited_ids.add(cur_user[0])
 
         # try to get friends to learn if the account was deleted
         try:
@@ -45,16 +48,19 @@ def walk_and_store(start=default_user, limit=5, lang=""):
         if score[0] >= SCORE_THRESHOLD:
             link = 'https://vk.com/id' + str(cur_user[0])
             programmers.loc[len(programmers)] = [*cur_user, *score, link]
-
-        if len(programmers) >= limit:
-            break
+            if len(programmers) >= limit:
+                break
 
         for friend in friends['items']:
-            friend = (friend['id'], friend['first_name'], friend['last_name'])
-            if friend[0] not in visited_ids:
-                to_visit.append(friend)
+            friend_t = (friend['id'], friend['first_name'], friend['last_name'])
+            if friend_t not in observed_users:
+                if friend_t[1] == 'Tukallo':
+                    print('fucking shit!')
+                to_visit.append(friend_t)
+                observed_users.add(friend_t)
 
     # bfs is finished, time to save programmers we found
+    print('All in all analyzed ' + str(len(observed_users)) + ' guys')
     programmers = programmers.sort_values(by=['rank'], ascending=False)
     programmers.to_csv(start[1] + start[2] + str(limit) + lang + ".csv")
 
